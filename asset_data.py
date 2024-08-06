19a65882b1af7ed69fae9518c3446695374b94af
Kevin <tokevinm@gmail.com>
2024-08-06 09:59:14 -0700

    Add files via upload

--- asset_data.py
+++ asset_data.py
@@ -1,21 +1,23 @@
 import os
 import requests
 from dotenv import load_dotenv
+load_dotenv()
 
 
 class DataManager:
+    """Requests, manages, and formats data received from the CoinGecko API"""
     def __init__(self):
 
         self.cg_endpoint = "https://api.coingecko.com/api/v3"
-        self.crypto_data = {}
+        self.asset_data = {}
         self.global_data = {}
         self.cg_header = {
             "x_cg_demo_api_key": os.environ["CG_API_KEY"],
         }
 
-    def update_price_data(self):
-
-        cg_params = {
+    def update_asset_data(self):
+        """API request to get relevant data for assets selected by users during sign-up"""
+        asset_params = {
             "ids": "bitcoin,ethereum,solana,dogecoin",
             "vs_currencies": "usd",
             "include_market_cap": "true",
@@ -23,17 +25,26 @@ class DataManager:
             "include_24h_vol": "true",
             "include_last_updated_at": "true",
         }
+        asset_response = requests.get(
+            url=f"{self.cg_endpoint}/simple/price",
+            headers=self.cg_header,
+            params=asset_params
+        )
+        asset_response.raise_for_status()
+        self.asset_data = asset_response.json()
 
-        crypto_response = requests.get(url=f"{self.cg_endpoint}/simple/price", params=cg_params, headers=self.cg_header)
-        crypto_response.raise_for_status()
-        self.crypto_data = crypto_response.json()
-
-        global_response = requests.get(f"{self.cg_endpoint}/global", headers=self.cg_header)
+    def update_global_data(self):
+        """API request to get data related to entire crypto market"""
+        global_response = requests.get(
+            url=f"{self.cg_endpoint}/global",
+            headers=self.cg_header
+        )
         global_response.raise_for_status()
         self.global_data = global_response.json()
 
     def format_data(self, asset):
-        asset_dict = {"price": '${:,.2f}'.format(self.crypto_data[asset]["usd"]),
-                      "mcap": '${:,.2f}'.format(self.crypto_data[asset]["usd_market_cap"]),
-                      "24h_perc": f"{round(self.crypto_data[asset]["usd_24h_change"], 2)}"}
+        """Formats data into a more readable format. Adds commas/punctuation and rounds to two decimal places."""
+        asset_dict = {"price": '${:,.2f}'.format(self.asset_data[asset]["usd"]),
+                      "mcap": '${:,.2f}'.format(self.asset_data[asset]["usd_market_cap"]),
+                      "24h_perc": f"{round(self.asset_data[asset]["usd_24h_change"], 2)}"}
         return asset_dict
