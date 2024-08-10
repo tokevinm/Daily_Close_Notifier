b6f74783730f0739c1d6f6c69c28c3be203170fa
Kevin <tokevinm@gmail.com>
2024-08-10 16:34:29 -0700

    Most recent changes 8/10

--- asset_data.py
+++ asset_data.py
@@ -9,31 +9,44 @@ class DataManager:
     def __init__(self):
 
         self.cg_endpoint = "https://api.coingecko.com/api/v3"
-        self.cg_asset_data = {}
+        self.cg_assets = "bitcoin,ethereum,solana,dogecoin"
+        self.cg_assets_list = self.cg_assets.split(",")
+        self.cg_assets_data = {}
         self.cg_global_data = {}
         self.crypto_mcap_perc_total = None
+        self.crypto_total_mcap = None
         # crypto_mcap_perc is a dictionary of top10 assets and percentages of total market cap
         self.cg_header = {
             "x_cg_demo_api_key": os.environ["CG_API_KEY"],
         }
 
-    def update_asset_data(self):
-        """API request to get relevant data for assets selected by users during sign-up"""
-        asset_params = {
-            "ids": "bitcoin,ethereum,solana,dogecoin",
-            "vs_currencies": "usd",
-            "include_market_cap": "true",
-            "include_24hr_change": "true",
-            "include_24h_vol": "true",
-            "include_last_updated_at": "true",
-        }
+    def get_asset_data(self, asset):
+        """Gets user requested data from CoinGecko API and formats it into a dictionary.
+        Also adds commas/punctuation and rounds to two decimal places."""
         asset_response = requests.get(
-            url=f"{self.cg_endpoint}/simple/price",
+            url=f"{self.cg_endpoint}/coins/{asset}",
             headers=self.cg_header,
-            params=asset_params
+            params={"id": asset}
         )
         asset_response.raise_for_status()
-        self.cg_asset_data = asset_response.json()
+        data = asset_response.json()
+        asset_dict = {
+            "ticker": data["symbol"],
+            "price": '${:,.2f}'.format(data["market_data"]["current_price"]["usd"]),
+            "mcap": '${:,.2f}'.format(data["market_data"]["market_cap"]["usd"]),
+            "total_volume": '${:,.2f}'.format(data["market_data"]["total_volume"]["usd"]),
+            "24h_change_usd": '${:,.2f}'.format(data["market_data"]["price_change_24h_in_currency"]["usd"]),
+            "24h_change_percent": f"{round(data["market_data"]["price_change_percentage_24h"], 2)}",
+            "7d_change_percent": f"{round(data["market_data"]["price_change_percentage_7d"], 2)}",
+            "30d_change_percent": f"{round(data["market_data"]["price_change_percentage_30d"], 2)}",
+        }
+        return asset_dict
+
+    def format_asset_data(self):
+        """Consolidates asset data into a single dictionary to be accessed"""
+        for i in range(len(self.cg_assets_list)):
+            self.cg_assets_data[self.cg_assets_list[i]] = self.get_asset_data(self.cg_assets_list[i])
+        return self.cg_assets_data
 
     def update_global_data(self):
         """API request to get data related to entire crypto market"""
@@ -43,24 +56,12 @@ class DataManager:
         )
         global_response.raise_for_status()
         self.cg_global_data = global_response.json()
-        self.crypto_mcap_perc_total = self.cg_global_data["data"]["market_cap_percentage"]
+        if self.cg_global_data["data"]["market_cap_percentage"]:
+            self.crypto_mcap_perc_total = self.cg_global_data["data"]["market_cap_percentage"]
+        if self.cg_global_data["data"]["total_market_cap"]["usd"]:
+            self.crypto_total_mcap = '${:,.2f}'.format(self.cg_global_data["data"]["total_market_cap"]["usd"])
 
-    def format_data(self, asset, asset_ticker):
-        """Formats data into a more readable format. Adds commas/punctuation and rounds to two decimal places."""
-        asset_dict = {"price": '${:,.2f}'.format(self.cg_asset_data[asset]["usd"]),
-                      "mcap": '${:,.2f}'.format(self.cg_asset_data[asset]["usd_market_cap"]),
-                      "24h_change_percent": f"{round(self.cg_asset_data[asset]["usd_24h_change"], 2)}",
-                      "percent_of_total_mcap": f"{round(self.crypto_mcap_perc_total[asset_ticker], 2)}%"
-                      }
-        return asset_dict
 
-    def daily_icon(self, percent_change):
-        """Takes the 24h % change in price and returns an icon to make it easier
-        to discern divergences between assets/asset classes"""
-        if float(percent_change) > 0:
-            icon = "🔺"
-        elif float(percent_change) < 0:
-            icon = "🔻"
-        else:
-            icon = ""
-        return icon
+if __name__ == '__main__':
+    bob = DataManager()
+    print(bob.get_asset_data("bitcoin"))
