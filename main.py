753630b76b3105dc53358dcee53b65d72b14ca8d
Kevin <tokevinm@gmail.com>
2024-08-09 18:00:30 -0700

    Commit

--- main.py
+++ main.py
@@ -1,57 +1,49 @@
-import asset_data
-import email_notifier
+from asset_data import DataManager
+from email_notifier import EmailNotifier
+from datetime import datetime
 
-data_manager = asset_data.DataManager()
+data_manager = DataManager()
 data_manager.update_asset_data()
 data_manager.update_global_data()
-global_data = data_manager.global_data
-email_manager = email_notifier.EmailNotifier()
+email_manager = EmailNotifier()
 users_data = email_manager.get_emails_data()
 
-total_crypto_mcap = '${:,.2f}'.format(global_data["data"]["total_market_cap"]["usd"])
-# Total crypto mcap
-crypto_mcap_perc_total = global_data["data"]["market_cap_percentage"]
-# crypto_mcap_perc is a dictionary of top10 assets and percentages of total market cap
+total_crypto_mcap = '${:,.2f}'.format(data_manager.cg_global_data["data"]["total_market_cap"]["usd"])
 
-btc_data = data_manager.format_data("bitcoin")
-btc_mcap_percent = f"{round(crypto_mcap_perc_total["btc"], 2)}%"
+btc_data = data_manager.format_data("bitcoin", "btc")
+eth_data = data_manager.format_data("ethereum", "eth")
+sol_data = data_manager.format_data("solana", "sol")
+doge_data = data_manager.format_data("dogecoin", "doge")
 
-eth_data = data_manager.format_data("ethereum")
-eth_mcap_percent = f"{round(crypto_mcap_perc_total["eth"], 2)}%"
+btc_icon = data_manager.daily_icon(btc_data["24h_change_percent"])
+eth_icon = data_manager.daily_icon(eth_data["24h_change_percent"])
+sol_icon = data_manager.daily_icon(sol_data["24h_change_percent"])
+doge_icon = data_manager.daily_icon(doge_data["24h_change_percent"])
 
-sol_data = data_manager.format_data("solana")
-sol_mcap_percent = f"{round(crypto_mcap_perc_total["sol"], 2)}%"
-
-doge_data = data_manager.format_data("dogecoin")
-doge_mcap_percent = f"{round(crypto_mcap_perc_total["doge"], 2)}%"
-
-
-up_down = None
-if float(btc_data["24h_perc"]) > 0:
-    up_down = "🔺"
-elif float(btc_data["24h_perc"]) < 0:
-    up_down = "🔻"
+day_of_month = datetime.now().strftime("%d")
+day_of_week = datetime.now().strftime("%w")
+if day_of_month == "01":
+    close_significance = "MONTHLY"
+elif day_of_week == "0":
+    close_significance = "WEEKLY"
 else:
-    up_down = ""
+    close_significance = "Daily"
 
 for user in users_data:
     user_email = user["pleaseEnterYourEmailAddress:"]
     user_option = user.get("anyExtraDataYou'dLikeInYourReport?", None)
-    email_message = (f"Subject:Bitcoin Daily Close: {btc_data["price"]} {up_down}{btc_data["24h_perc"]}% \n\n"
-                     f"BTC Market Cap: {btc_data["mcap"]}\n\n")
+    email_message = (f"Subject:BTC {close_significance} Close: "
+                     f"{btc_data["price"]} {btc_icon}{btc_data["24h_change_percent"]}%\n\n")
     if user_option is not None:
         user_choices = user_option.split()
         if "Ethereum" in user_choices:
-            message_add = (f"Ethereum Price: {eth_data["price"]}\n"
-                           f"   ETH Market Cap: {eth_data["mcap"]}\n")
+            message_add = f"ETH: {eth_data["price"]} {eth_icon}{eth_data["24h_change_percent"]}%\n"
             email_message += message_add
         if "Solana" in user_choices:
-            message_add = (f"Solana Price: {sol_data["price"]}\n"
-                           f"   SOL Market Cap: {sol_data["mcap"]}\n")
+            message_add = f"SOL: {sol_data["price"]} {sol_icon}{sol_data["24h_change_percent"]}%\n"
             email_message += message_add
         if "Dogecoin" in user_choices:
-            message_add = (f"Dogecoin Price: {doge_data["price"]}\n"
-                           f"   DOGE Market Cap: {doge_data["mcap"]}\n")
+            message_add = f"DOGE: {doge_data["price"]} {doge_icon}{doge_data["24h_change_percent"]}%\n"
             email_message += message_add
-    email_message += f"\n Total Cryptocurrency Market Cap: {total_crypto_mcap}"
+    email_message += f"\nBTC Market Cap: \n{btc_data["mcap"]}\nTotal Cryptocurrency Market Cap: \n{total_crypto_mcap}"
     email_manager.send_emails(user_email, email_message.encode('utf-8'))
