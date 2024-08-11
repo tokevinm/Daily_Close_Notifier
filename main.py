c783f690c18c060cf06942c6c557883db2f4d55d
Kevin <tokevinm@gmail.com>
2024-08-11 11:11:15 -0700

    Most recent changes 8/10

--- main.py
+++ main.py
@@ -11,22 +11,18 @@ email_man = EmailNotifier()
 users_data = email_man.get_emails_data()
 
 
-def daily_icon(percent_change):
+def daily_up_down_icon(percent_change):
     """Takes the 24h % change in price and returns an icon to make it easier
     to discern divergences between assets/asset classes"""
-    if float(percent_change) > 0:
+    percent = float(percent_change)
+    if percent > 0:
         icon = "🔺"
-    elif float(percent_change) < 0:
+    elif percent < 0:
         icon = "🔻"
     else:
         icon = ""
     return icon
 
-
-btc_icon = daily_icon(asset_data["bitcoin"]["24h_change_percent"])
-eth_icon = daily_icon(asset_data["ethereum"]["24h_change_percent"])
-sol_icon = daily_icon(asset_data["solana"]["24h_change_percent"])
-doge_icon = daily_icon(asset_data["dogecoin"]["24h_change_percent"])
 
 day_of_month = datetime.now().strftime("%d")
 day_of_week = datetime.now().strftime("%w")
@@ -39,23 +35,19 @@ else:
 
 for user in users_data:
     user_email = user["pleaseEnterYourEmailAddress:"]
-    user_option = user.get("anyExtraDataYou'dLikeInYourReport?", None)
+    user_options = user.get("anyExtraDataYou'dLikeInYourReport?", None)
+    btc_up_down = daily_up_down_icon(asset_data["bitcoin"]["24h_change_percent"])
     message_body = (f"Subject:BTC {close_significance} Close: "
-                    f"{asset_data["bitcoin"]["price"]} {btc_icon}{asset_data["bitcoin"]["24h_change_percent"]}%\n\n")
-    if user_option is not None:
-        user_choices = user_option.split()
-        if "Ethereum" in user_choices:
-            message_add = (f"ETH: {asset_data["ethereum"]["price"]} "
-                           f"{eth_icon}{asset_data["ethereum"]["24h_change_percent"]}%\n")
+                    f"{asset_data["bitcoin"]["price"]} {btc_up_down}{asset_data["bitcoin"]["24h_change_percent"]}%\n\n")
+    if user_options is not None:
+        user_choices = user_options.split()[::2]
+        for choice in user_choices:
+            asset_lower = choice.lower()
+            asset_up_down = daily_up_down_icon(asset_data[asset_lower]["24h_change_percent"])
+            message_add = (f"{asset_data[asset_lower]["ticker_upper"]}: {asset_data[asset_lower]["price"]} "
+                           f"{asset_up_down}{asset_data[asset_lower]["24h_change_percent"]}%\n")
             message_body += message_add
-        if "Solana" in user_choices:
-            message_add = (f"SOL: {asset_data["solana"]["price"]} "
-                           f"{sol_icon}{asset_data["solana"]["24h_change_percent"]}%\n")
-            message_body += message_add
-        if "Dogecoin" in user_choices:
-            message_add = (f"DOGE: {asset_data["dogecoin"]["price"]} "
-                           f"{doge_icon}{asset_data["dogecoin"]["24h_change_percent"]}%\n")
-            message_body += message_add
-    message_body += (f"\nBTC Market Cap: \n{asset_data["bitcoin"]["mcap"]}\n"
+        message_body += "\n"
+    message_body += (f"BTC Market Cap: \n{asset_data["bitcoin"]["mcap"]}\n"
                      f"Total Cryptocurrency Market Cap: \n{data_man.crypto_total_mcap}")
     email_man.send_emails(user_email, message_body.encode('utf-8'))
