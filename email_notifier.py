9328471e4183e57a21abe1114d6668ba856c0280
Kevin <tokevinm@gmail.com>
2024-09-10 15:11:28 -0700

    Add html support for future updates, add unsubscribe/update preferences feature

--- email_notifier.py
+++ email_notifier.py
@@ -8,17 +8,17 @@ load_dotenv()
 class EmailNotifier:
     """Handles API request to receive user data from Google Docs and sends requested data to users via email SMTP"""
     def __init__(self):
-        self._smtp_username = os.environ["SMTP_USERNAME"]
+        self.smtp_username = os.environ["SMTP_USERNAME"]
         self._smtp_password = os.environ["SMTP_PASSWORD"]
         self._sheety_endpoint = os.environ["SHEETY_USERS_ENDPOINT"]
-        self.sheety_header = {"Authorization": os.environ["SHEETY_BEARER"]}
+        self._sheety_header = {"Authorization": os.environ["SHEETY_BEARER"]}
         self.users_data = []
 
     def get_emails_data(self):
-        """API request to get stored user data"""
+        """API request to get all stored user data"""
         users_response = requests.get(
             self._sheety_endpoint,
-            headers=self.sheety_header
+            headers=self._sheety_header
         )
         users_response.raise_for_status()
         self.users_data = users_response.json()["users"]
@@ -30,9 +30,9 @@ class EmailNotifier:
         try:
             with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
                 connection.starttls()
-                connection.login(user=self._smtp_username, password=self._smtp_password)
+                connection.login(user=self.smtp_username, password=self._smtp_password)
                 result = connection.sendmail(
-                    from_addr=self._smtp_username,
+                    from_addr=self.smtp_username,
                     to_addrs="k1wsnt@gmail.com",
                     msg=message
                 )
